51fb8080f42a547938dd43089d4278faa7c5bb9a
TRustHvH <vpolakov88@gmail.com>
2024-01-09 18:32:12 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -4,6 +4,50 @@ window.addEventListener("mousewheel", function(e) {
     return false;
 }
 }, {passive:false});
-
-
+// function decreaseSize() {
+//     const images = document.getElementsByTagName('img');
+//     const svgs = document.getElementsByTagName('svg');
+//
+//     for (let i = 0; i < images.length; i++) {
+//         const currentWidth = images[i].width;
+//         images[i].style.width = `${currentWidth - 1}px`;
+//     }
+//
+//     for (let i = 0; i < svgs.length; i++) {
+//         const currentWidth = svgs[i].getAttribute('width');
+//         svgs[i].setAttribute('width', `${parseInt(currentWidth) - 1}px`);
+//     }
+// }
+//
+// // Функция для увеличения размера элементов на 1px
+// function increaseSize() {
+//     const images = document.getElementsByTagName('img');
+//     const svgs = document.getElementsByTagName('svg');
+//
+//     for (let i = 0; i < images.length; i++) {
+//         const currentWidth = images[i].width;
+//         images[i].style.width = `${currentWidth + 1}px`;
+//
+//     }
+//
+//     for (let i = 0; i < svgs.length; i++) {
+//         const currentWidth = svgs[i].getAttribute('width');
+//         svgs[i].setAttribute('width', `${parseInt(currentWidth) + 1}px`);
+//     }
+// }
+//
+// window.innerWidthDelta = window.innerWidth;
+//
+// window.addEventListener('resize', () => {
+//     const currentWidth = window.innerWidth;
+//     const previousWidth = window.innerWidthDelta;
+//
+//     if (currentWidth < previousWidth) {
+//         decreaseSize();
+//     } else if (currentWidth > previousWidth) {
+//         increaseSize();
+//     }
+//
+//     window.innerWidthDelta = currentWidth;
+// });
 

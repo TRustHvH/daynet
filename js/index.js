4dc0d175789311b54df07139558557ee18815951
TRustHvH <vpolakov88@gmail.com>
2024-02-03 06:33:11 +0300

    720 full fix big changes and etc.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,24 @@ window.addEventListener("mousewheel", function(e) {
     return false;
 }
 }, {passive:false});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let mobile_menu = document.getElementById('mobile-menu')
+    let is_opened = false
+
+    function mobile_menu_opener() {
+        if (is_opened === false) {
+            mobile_menu.style.display = 'flex'
+            is_opened = true
+        } else {
+            mobile_menu.style.display = 'none'
+            is_opened = false
+        }
+    }
+
+    document.getElementById("mobile-header-button").addEventListener("click", mobile_menu_opener);
+    document.getElementById("close-mobile-header").addEventListener("click", mobile_menu_opener);
+});
 // function decreaseSize() {
 //     const images = document.getElementsByTagName('img');
 //     const svgs = document.getElementsByTagName('svg');

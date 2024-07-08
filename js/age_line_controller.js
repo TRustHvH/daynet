a4acadbbe73d3588ba138782fa3b7fe53119edb5
TRustHvH <vpolakov88@gmail.com>
2024-07-08 19:28:57 +0300

    some animations to daynet line

--- js/age_line_controller.js
+++ js/age_line_controller.js
@@ -7,14 +7,18 @@ $(document).ready(function(){
   $(".line-circle").on(
     {
       mouseenter: function () {
-        let age = $(this).attr('age')
-        $(`.age-${age}-line`).css("display", "block")
-        $(`.age-${age}-text`).css("display", "block")
+            let age = $(this).attr('age')
+            $(`.age-${age}-line`).css("display", "block")
+            $(setTimeout(function () { $(`.age-${age}-line`).css("opacity", "1") }, 1))
+            $(`.age-${age}-text`).css("display", "block")
+            $(setTimeout(function () { $(`.age-${age}-text`).css("opacity", "1") }, 1))
       },
       mouseleave: function () {
-        let age = $(this).attr('age')
-        $(`.age-${age}-line`).css("display", "none")
-        $(`.age-${age}-text`).css("display", "none")
+            let age = $(this).attr('age')
+            $(`.age-${age}-line`).css("display", "none")
+            $(setTimeout(function () { $(`.age-${age}-line`).css("opacity", "0")}, 1))
+            $(`.age-${age}-text`).css("display", "none")
+            $(setTimeout(function () { $(`.age-${age}-text`).css("opacity", "0") }, 1))
       }
     }
   )

5f09da2f6b01412e38225082b461a323306b7d25
Streidant <nikyanovich@gmail.com>
2024-07-16 10:03:57 +0300

    добавил сворачивания хедера при скролле вниз и возвращение в исходное положение при скролле вверх

--- js/index.js
+++ js/index.js
@@ -21,11 +21,33 @@ document.addEventListener("DOMContentLoaded", function() {
     /// --- Header Animation --- \\\
     
     document.addEventListener("scroll", () => {
-        if (window.scrollY >= 85) {
+        if (window.scrollY >= 220) {
             document.querySelector(".header").classList.add("header-scroll");
         } else {
             document.querySelector(".header").classList.remove("header-scroll");
         }
+    })
+
+    /// --- Header-hide --- \\\
+
+    let lastScroll = 0;
+    const defaultOffset = 200;
+    const header = document.querySelector('.header');
+
+    const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+    const containHide = () => header.classList.contains('hide');
+
+    window.addEventListener('scroll', () => {
+        if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+            //scroll down
+            header.classList.add('hide');
+        }
+        else if(scrollPosition() < lastScroll && containHide()){
+            //scroll up
+            header.classList.remove('hide');
+        }
+
+        lastScroll = scrollPosition();
     })
 
     /// --- Popup menu --- \\\

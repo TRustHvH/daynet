b68c527a99d85422193cec6ebd0ac0e2b3fae334
Streidant <nikyanovich@gmail.com>
2024-07-20 16:26:10 +0300

    Скрытие и показ текста при навденении курсора на формы ОС (сделано на всех страницах)

--- js/index.js
+++ js/index.js
@@ -127,4 +127,28 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const ids = [
+        'phoneInput',
+        'nameInput',
+        'messageInput',
+        'sphereInput',
+        'siteInput',
+        'commentInput'
+    ];
+    ids.forEach(id => {
+        const input = document.getElementById(id);
+        if (input) {
+            input.addEventListener('mouseover', function() {
+                this.setAttribute('data-placeholder', this.placeholder);
+                this.placeholder = ''; // Очищаем placeholder
+            });
+            input.addEventListener('mouseout', function() {
+                this.placeholder = this.getAttribute('data-placeholder');
+            });
+        }
+    });
+});
+
+
 
